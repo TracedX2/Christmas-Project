bb3b47533fc2e19b495dca4f87c9bbc1044928fd
cl20gryboskie@hsonetone.clsd.net <cl20gryboskie@hsonetone.clsd.net>
2017-12-18 11:46:47 -0500

    fixed many tings

--- christmasProject.java
+++ christmasProject.java
@@ -16,14 +16,15 @@ public class christmasProject
         PrintWriter giftWriter = new PrintWriter(new File("giftsResults.txt"));
         double budget = 0;
         double spent = 0;
+        int daysLeft = 0;
         ArrayList<Double> cost = new ArrayList<Double>();
         ArrayList<Integer> min = new ArrayList<Integer>();
         ArrayList<Integer> max = new ArrayList<Integer>();
-        int days = 0;
         ArrayList<Integer> age = new ArrayList<Integer>();
         ArrayList<String> description = new ArrayList<String>();
         ArrayList<String> name = new ArrayList<String>();
         ArrayList<String> behavior = new ArrayList<String>();
+        ArrayList<Integer> days = new ArrayList<Integer>();
         String n;
 
 
@@ -34,23 +35,19 @@ public class christmasProject
 	   		String d = giftReader.nextLine();
             	description.add(d);
 	   		int mi = giftReader.nextInt();
+	   			reader.nextLine();
             	min.add(mi);
+            int maxAge = giftReader.nextInt();
+            	reader.nextLine();
+            	max.add(maxAge);
+            double price = giftReader.nextDouble();
+            	reader.nextLine();
+            	cost.add(price);
+            int time = giftReader.nextInt();
+            	days.add((Integer)time);
+            	giftReader.nextLine();
 
-             //Get scanner instance
-		Scanner scanner = new Scanner(new File("gifts.txt"));
 
-		//Set the delimiter used in file
-		scanner.useDelimiter(",");
-
-		//Get all tokens and store them in some data structure
-		//I am just printing them
-		while (scanner.hasNext())
-		{
-		    System.out.println (scanner.next());
-		}
-
-		//Do not forget to close the scanner
-        scanner.close();
 		}
            /* max.add(n);
             cost.add(n);
@@ -65,7 +62,7 @@ public class christmasProject
         System.out.println("What is your budget?");
         budget = reader.nextDouble();
         System.out.println("When would you like these items by?");
-        days = reader.nextInt();
+        daysLeft = reader.nextInt();
 
 }
 }
