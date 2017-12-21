02906663e549edabeda53d5594a7627e1e24a9f0
cl20gryboskie@hsonetone.clsd.net <cl20gryboskie@hsonetone.clsd.net>
2017-12-21 11:54:01 -0500

    parse ints are now added

--- christmasProject.java
+++ christmasProject.java
@@ -22,9 +22,7 @@ public class christmasProject
         //EG - created array lists to hold all of the data
         ArrayList<Double> cost = new ArrayList<Double>();
         ArrayList<String> minS = new ArrayList<String>();
-        ArrayList<Integer> minint = new ArrayList<Integer>();
         ArrayList<Integer> max = new ArrayList<Integer>();
-        ArrayList<Integer> age = new ArrayList<Integer>();
         ArrayList<String> description = new ArrayList<String>();
         ArrayList<String> name = new ArrayList<String>();
         ArrayList<String> behavior = new ArrayList<String>();
@@ -38,16 +36,23 @@ public class christmasProject
 			//EG - brings in items from the list and adds it to the Arrays
 	   		String d = giftReader.nextLine();
             	description.add(d);
+
 	   		String mi = giftReader.nextLine();
+	   		int minInt = Integer.parseInt(mi);
 	   			minS.add(mi);
-	   			/*
+
             String maxAge = giftReader.nextLine();
-            	max.add(maxAge);
+            int ageInt = Integer.parseInt(maxAge);
+            	max.add(ageInt);
+
             String price = giftReader.nextLine();
-            	cost.add(price);
+            double dollars = Double.parseDouble(price);
+            	cost.add(dollars);
+
             String time = giftReader.nextLine();
-            	days.add(time);
-            	*/
+            int amountOfDays = Integer.parseInt(time);
+            	days.add(amountOfDays);
+
 
 
 
@@ -65,7 +70,7 @@ public class christmasProject
         budget.add(g);
         System.out.println("When would you like these items by?");
         daysLeft = reader.nextInt();
-	
+
 	//cut people from the gift list
         if(age <= 15)
         {
@@ -89,7 +94,7 @@ public class christmasProject
             }
         }
 
-<<<<<<< HEAD
+
 	//cut people from the gift list
         if(age <= 15)
         {
@@ -113,8 +118,6 @@ public class christmasProject
             }
         }
 
-=======
->>>>>>> 0927ce69cf89c890c376242bb62711ca159827f2
         if(budget.equals(0))
         {
             System.out.println ("Santa has run out of money");
