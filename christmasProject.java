4796e27fd29fdab44913c1697c931bdd09f27908
rjfire7 <rjfire7@gmail.com>
2017-12-21 12:00:41 -0500

    fixed errors and chagned budget to double

--- christmasProject.java
+++ christmasProject.java
@@ -16,13 +16,16 @@ public class christmasProject
         Scanner reader = new Scanner (System.in);
         PrintWriter kidWriter = new PrintWriter(new File("kidsResults.txt"));
         PrintWriter giftWriter = new PrintWriter(new File("giftsResults.txt"));
-        ArrayList<Double> budget = new ArrayList<Double>();
+        //RF - changed budget from ArrayList to a double
+        double budget;
         double spent = 0;
         int daysLeft = 0;
         //EG - created array lists to hold all of the data
         ArrayList<Double> cost = new ArrayList<Double>();
         ArrayList<String> minS = new ArrayList<String>();
+        ArrayList<Integer> minint = new ArrayList<Integer>();
         ArrayList<Integer> max = new ArrayList<Integer>();
+        ArrayList<Integer> age = new ArrayList<Integer>();
         ArrayList<String> description = new ArrayList<String>();
         ArrayList<String> name = new ArrayList<String>();
         ArrayList<String> behavior = new ArrayList<String>();
@@ -36,23 +39,16 @@ public class christmasProject
 			//EG - brings in items from the list and adds it to the Arrays
 	   		String d = giftReader.nextLine();
             	description.add(d);
-
 	   		String mi = giftReader.nextLine();
-	   		int minInt = Integer.parseInt(mi);
 	   			minS.add(mi);
-
+	   			/*
             String maxAge = giftReader.nextLine();
-            int ageInt = Integer.parseInt(maxAge);
-            	max.add(ageInt);
-
+            	max.add(maxAge);
             String price = giftReader.nextLine();
-            double dollars = Double.parseDouble(price);
-            	cost.add(dollars);
-
+            	cost.add(price);
             String time = giftReader.nextLine();
-            int amountOfDays = Integer.parseInt(time);
-            	days.add(amountOfDays);
-
+            	days.add(time);
+            	*/
 
 
 
@@ -66,59 +62,84 @@ public class christmasProject
 
         //get info from user
         System.out.println("What is your budget?");
-        double g = reader.nextDouble();
-        budget.add(g);
+        budget = reader.nextDouble();
         System.out.println("When would you like these items by?");
         daysLeft = reader.nextInt();
 
 	//cut people from the gift list
-        if(age <= 15)
-        {
-            budget = budget - cost;
-        }
-        while(!budget.equals(0))
+	//RF - used for each loops to loop through ArrayLists and cut people from the lists and changed some bad operand errors
+    for(int i: age)
+    {
+		for(double d: cost)
+		{
+       		if(i <= 15)
+       		{
+            	budget = budget - d;
+        	}
+		}
+	}
+        while(budget != 0)
         {
             for(int a = 16; a <= 18; a++)
             {
                 if(age.equals(a) && behavior.equals("nice"))
                 {
-                    budget = budget - cost;
+					for(double d: cost)
+					{
+                    	budget = budget - d;
+					}
                 }
             }
             for(int b = 16; b <= 18; b++)
             {
                 if(age.equals(b) && behavior.equals("naughty"))
                 {
-                    budget = budget - cost;
+					//RF - for each loop
+					for(double d: cost)
+					{
+                    	budget = budget - d;
+					}
                 }
             }
         }
-
-
 	//cut people from the gift list
-        if(age <= 15)
+    for(int i: age)
+    {
+        if(i <= 15)
         {
-            budget = budget - cost;
+			//RF - for each loop
+			for(double d: cost)
+			{
+            	budget = budget - d;
+			}
         }
-        while(!budget.equals(0))
+	}
+        while(budget != 0)
         {
             for(int a = 16; a <= 18; a++)
             {
                 if(age.equals(a) && behavior.equals("nice"))
                 {
-                    budget = budget - cost;
+					//RF - for each loop
+					for(double d: cost)
+					{
+                   		budget = budget - d;
+					}
                 }
             }
             for(int b = 16; b <= 18; b++)
             {
                 if(age.equals(b) && behavior.equals("naughty"))
                 {
-                    budget = budget - cost;
+					//RF - for each loop
+					for(double d: cost)
+					{
+                    	budget = budget - d;
+					}
                 }
             }
         }
-
-        if(budget.equals(0))
+        if(budget == 0)
         {
             System.out.println ("Santa has run out of money");
         }
