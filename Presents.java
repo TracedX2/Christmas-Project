cea33298a09f9a5fb4a1101dc549a747ac7e3413
Eli <egryboski@gmail.com>
2018-01-03 21:49:08 -0500

    Made presents class

--- Presents.java
+++ Presents.java
@@ -1,2 +1,60 @@
+
+//MADE BY EG - creates a present object to do stuff with
+
 public class Presents
 {
+	//instance variables
+	private String toyName;
+	private int minAge;
+	private int maxAge;
+	private double price;
+	private int daysToMake;
+
+	//constructor to initialize instance variables
+	public Presents(String n, int min, int max, double p, int d)
+	{
+		toyName = n;
+		minAge = min;
+		maxAge = max;
+		price = p;
+		daysToMake = d;
+	}
+
+	//get methods
+	public String getName ()
+	{
+		return toyName;
+	}
+
+	public int getMinAge ()
+	{
+		return minAge;
+	}
+
+	public int getMaxAge ()
+	{
+		return maxAge;
+	}
+
+	public double getPrice()
+	{
+		return price;
+	}
+
+	public int getDays()
+	{
+		return daysToMake;
+	}
+
+	//puts info into a string
+	public String toString ()
+	{
+		String str = "\nToy:  " + toyName;
+		str += "\nMinimum age:  " + minAge;
+		str += "\nMaximum age:  " + maxAge;
+		str += "\nPrice:  " + price;
+		str += "\nDays to make:  " + daysToMake;
+
+		return str;
+	}
+}
---
+++ testerforthings.java
@@ -0,0 +1,12 @@
+
+//EG used to test the classes
+
+public class testerforthings
+{
+	public static void main (String[]args)
+	{
+		Presents firstGift = new Presents("Twister", 5, 80, 9.99, 3);
+
+		System.out.println(firstGift);
+	}
+}
