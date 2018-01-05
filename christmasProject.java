18a7cb750be7fb4912d6e0e81e6cbbcf99d8fd7b
Eli <egryboski@gmail.com>
2018-01-04 21:16:47 -0500

    adding the objects to array lists

--- christmasProject.java
+++ christmasProject.java
@@ -47,12 +47,12 @@ public class christmasProject
 	    while(giftReader.hasNext())
 	    {
 	   	//EG - brings in items from the list and adds it to the Arrays
-	   	String d = giftReader.nextLine();
-	        	description.add(d);
+	   		String d = giftReader.nextLine();
+	    	    	description.add(d);
 
-	   	String mi = giftReader.nextLine();
-	   	int minInt = Integer.parseInt(mi);
-	   		minS.add(mi);
+	   		String mi = giftReader.nextLine();
+	   		int minInt = Integer.parseInt(mi);
+	   			minS.add(mi);
 
 	        String maxAge = giftReader.nextLine();
 	        int ageInt = Integer.parseInt(maxAge);
@@ -67,18 +67,31 @@ public class christmasProject
 	        int amountOfDays = Integer.parseInt(time);
 	        	days.add(amountOfDays);
 
+			Presents present1 = new Presents(d, minInt, ageInt, dollars, amountOfDays);
+
+			presentList.add(present1);
 
 
 		}
 
 		System.out.println(totalCost + "Total cost is right here ");
+		System.out.println(presentList);
 
 		//EG - Started the while loop to add the kids
 		//EG - thinking about creating other files to have objects like a present object and a child object
 		while(kidReader.hasNext())
 		{
-			String kid = kidReader.nextLine();
-			kids.add(kid);
+			String kidName;
+			String norn;
+			int kidAge;
+			Scanner K = new Scanner("kids.txt").useDelimiter(",");
+
+			kidName = K.nextLine();
+			norn = K.nextLine();
+			kidAge = K.nextInt();
+			System.out.println(kidName);
+			System.out.println(norn);
+			System.out.println(kidAge);
 		}
 		//end EG
 
